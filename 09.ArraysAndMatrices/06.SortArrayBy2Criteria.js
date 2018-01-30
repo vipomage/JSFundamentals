d85ec9d663d8e69454f4a2f63917c6f523b5f0aa
vipomage <vipomage@gmail.com>
2018-01-30 22:32:02 +0200

    fix empty lines and practice Git

--- 09.ArraysAndMatrices/06.SortArrayBy2Criteria.js
+++ 09.ArraysAndMatrices/06.SortArrayBy2Criteria.js
@@ -1,8 +1,6 @@
 function sortArrByCriteria( arr ) {
     return arr.sort( ( a, b ) => a >= b ).sort( ( a, b ) => a.length - b.length ).join( '\n' ) //YIS!
 }
-
-
 console.log( sortArrByCriteria( [ 'alpha', 'beta', 'gamma' ] ) );
 console.log( sortArrByCriteria( [ 'Isacc', 'Theodor', 'Jack', 'Harrison', 'George' ] ) );
 console.log( sortArrByCriteria( [ 'test', 'Deny', 'omen', 'Default' ] ) );
