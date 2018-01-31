c5cb73f36398d636e91ad3d1187e272c3b2e2c6f
vipomage <vipomage@gmail.com>
2018-01-31 06:31:36 +0200

    02. from Strings and Regular Expressions

---
+++ 10.StringsAndRegEx/02.ConcatenateReversed.js
@@ -0,0 +1,11 @@
+function concReversed(arr) {
+    let array = arr.reverse();
+    let tempArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        tempArray.push(arr[i].split('').reverse().join(''))
+
+    }
+    return tempArray.join('')
+}
+
+console.log(concReversed(['I', 'am', 'student']));
