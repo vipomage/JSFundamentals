5c243181d2a250aeabb2da0855821a3aecf37665
vipomage <vipomage@gmail.com>
2018-02-01 17:12:51 +0200

    03. from Strings and Regular Expressions

--- 10.StringsAndRegEx/03.CountOccurrences.js
+++ 10.StringsAndRegEx/03.CountOccurrences.js
@@ -7,7 +7,6 @@ function countStringInText(str, text) {
     }
     return count
 }
-
 console.log(countStringInText('ma', 'Marine mammal training is the training and caring for marine life such as, dolphins, killer whales, sea lions,' +
     ' walruses, and other marine mammals. It is also a duty of the trainer to do mental and physical exercises to keep the animal healthy and happy.'));
 console.log(countStringInText('the', 'The quick brown fox jumps over the lay dog.'));
