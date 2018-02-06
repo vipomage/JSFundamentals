648171601eb37160263a85c9ecef3ed42c806a2c
vipomage <vipomage@gmail.com>
2018-02-06 09:48:04 +0200

    08. Objects and JSON

--- 00/sortings.js
+++ 00/sortings.js
@@ -1,16 +1,16 @@
 /*
-
+--------------------------------------------------------------------------------------------------
 ---  count/size descending order  10 - 9 - 8 ....
 
 .sort((a,b)=> a < b))
 
-
+--------------------------------------------------------------------------------------------------
 --- Lexicographically/Alphabetically A - > B
 
 .sort((a,b)=> a > b)
 .sort()
 
-
+--------------------------------------------------------------------------------------------------
 ------Order by number ascending then by alphabetical
 
 if(a !== b) {
@@ -18,7 +18,12 @@ if(a !== b) {
         } else {
             return a.localeCompare(b); //string
         }
+-------------------------------------------------------------------------------------------------
+in ascending order based on their length, if two arrays have the same length they should be printed
+in order of being received from the input. Each individual array should be printed in descending order
 
+let customSort = (arrA, arrB, map) => map.get(arrA) - map.get(arrB);
+--------------------------------------------------------------------------------------------------
 
 first criteria goes last ...
  */
